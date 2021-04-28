e71111bb686a27bb8fe54161b5fcf6cdd7ea7821
Thales Freitas <thalesfreitas10@gmail.com>
2021-04-28 18:03:34 -0300

    Componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -16,16 +17,13 @@ export default function Home() {
             Get access to all the publications <br/>
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
 
       </main>
-
-    <h1>
-      Hello World
-    </h1>
-    
+   
     </>
   )
 }
