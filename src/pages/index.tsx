74a32e825dcff32dc1a246e25e4a6b7a54c777c1
Thales Freitas <thalesfreitas10@gmail.com>
2021-05-07 11:40:55 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 import Head from 'next/head';
 import {stripe} from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+
+// três formas de popular uma página com informações:
+// Client-side
+// Server-side
+// Static Site Generation
+
+
 
 interface HomeProps {
   product: {
@@ -38,7 +45,7 @@ export default function Home({ product }:HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   //se colocar um console.log não ira exibir, pois isso está na camada
   // do next e não do cliente
  
@@ -55,7 +62,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
 
