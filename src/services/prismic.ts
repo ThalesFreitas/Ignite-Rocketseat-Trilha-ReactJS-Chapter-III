0b8362488ab4e3148b49d451d06e5a3f3b8428fb
Thales Freitas <thalesfreitas10@gmail.com>
2022-03-12 12:00:21 -0300

    Consumindo API do Prismic

---
+++ src/services/prismic.ts
@@ -0,0 +1,13 @@
+const Prismic = require('@prismicio/client');
+
+export function getPrismicClient(req?: unknown) {
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
+        {
+            req,
+            accessToken: process.env.PRISMIC_ACCESS_TOKEN
+        }
+    )
+
+    return prismic;
+}
